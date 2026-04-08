8ab553ea6ddfaa13db47efc099aade4cef4685e0
Eric Hester <eric.hester@umbrella.tech>
2026-04-08 19:08:31 -0400

    Fix VMware physical NIC mapping

--- tests/test_vmware_mapping.py
+++ tests/test_vmware_mapping.py
@@ -19,3 +19,27 @@ class TestVMwareExamplePlatformPrereqs:
         vm_platform_prereqs = [prereq for name, prereq in platform_prereqs if name == "vm"]
         assert len(vm_platform_prereqs) == 1
         assert vm_platform_prereqs[0].args["name"] == "source('guest.guestFullName') or 'Unknown'"
+
+
+class TestVMwareExamplePhysicalNicMapping:
+    def test_host_physical_nics_include_host_context_for_description(self):
+        cfg = load_config("mappings/vmware.hcl.example")
+        host_obj = next(o for o in cfg.objects if o.name == "host")
+        physical_nic_block = host_obj.interfaces[0]
+
+        assert (
+            physical_nic_block.source_items
+            == "[{'_nic': nic, '_host_name': source('name')} for nic in (source('config.network.pnic') or [])]"
+        )
+
+        fields = {f.name: f for f in physical_nic_block.fields}
+        assert fields["name"].value == "source('_nic.device')"
+        assert fields["mac_address"].value == "upper(source('_nic.mac'))"
+        assert fields["type"].value == (
+            "map_value(source('_nic.linkSpeed.speedMb'), {1000: '1000base-t', 10000: '10gbase-x-sfpp', 25000: '25gbase-x-sfp28', 40000: '40gbase-x-qsfpp', 100000: '100gbase-x-qsfp28'}, '1000base-t')"
+        )
+        assert fields["speed"].value == "source('_nic.linkSpeed.speedMb')"
+        assert fields["duplex"].value == (
+            "map_value(source('_nic.linkSpeed.duplex'), {True: 'full', False: 'half'}, None)"
+        )
+        assert fields["description"].value == "join(' ', [source('_host_name'), source('_nic.device')])"
